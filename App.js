614e170cec2106d40d94877df9085e1278c3f9a7
Abdullah Mzaien <xa300600@hotmail.com>
2024-01-19 22:55:16 +0300

    chore: adding non-working example

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useFonts } from "expo-font";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    "Inter-SemiBold": require("./assets/fonts/Inter-SemiBold.ttf"),
+  });
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{ fontFamily: "Inter-SemiBold" }}>Inter Black</Text>
       <StatusBar style="auto" />
     </View>
   );
